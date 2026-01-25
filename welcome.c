3826ea51f438548ec2c5b37d4c23863d11fb5e87
Andy Firmansyah <dw201366@gmail.com>
2026-01-25 13:02:29 +0700

    Refactor greeting handling and improve memory management

--- welcome.c
+++ welcome.c
@@ -2,31 +2,45 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main() {
+  
+  /* Get Current Time */
   time_t currentTime;
   struct tm *localTime;
   int current_hour;
-  const char greetings[3][34] = {"Good Morning", "Good Evening", "Good Afternoon"};
-  char greetings_time[24];
-  greetings_time[0] = '\0';
+
+  /* Get Greeting */
+  const char *greetings[] = {"Good Morning", "Good Evening", "Good Afternoon"};
+
+  char *greetings_output;
+  greetings_output = malloc(24 * sizeof(char));
+  greetings_output[0] = '\0';
+
   time(&currentTime);
   localTime = localtime(&currentTime);
   current_hour = localTime->tm_hour;
+
+  /* Process for getting right grettings */
   if (current_hour >= 5 && current_hour < 12) {
-    strcpy(greetings_time, greetings[0]);
+    strcpy(greetings_output, greetings[0]);
   } else if (current_hour >= 12 && current_hour < 17) {
-    strcpy(greetings_time, greetings[2]);
+    strcpy(greetings_output, greetings[2]);
   } else {
-    strcpy(greetings_time, greetings[1]);
+    strcpy(greetings_output, greetings[1]);
   }
 
+  /* Get username */
   int getlogin_r(char *buf, size_t bufsize);
   char username[256];
+
+  /* Print out! */
   if (getlogin_r(username, sizeof(username)) == 0) {
-    printf("%s, %s!. Today Time and Date: %d-%02d-%d %02d:%02d:%02d\n", greetings_time, username, localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year + 1900, localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
+    printf("%s, %s!. Today Time and Date: %d-%02d-%d %02d:%02d:%02d\n", greetings_output, username, localTime->tm_mday, localTime->tm_mon + 1, localTime->tm_year + 1900, localTime->tm_hour, localTime->tm_min, localTime->tm_sec);
   } else {
     printf("Can't get the username!\n");
   }
-  return 0;
+  
+  return EXIT_SUCCESS;
 }
